64d9cafb6c0474bda7c0a6a4f24d412fd2d0e85a
Gergely Bihary <gergely.bihary@prezi.com>
2022-03-12 18:54:15 +0100

    feat: Better hierarchy on landing page

--- src/app/landing/header/header.component.ts
+++ src/app/landing/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  isOpen = true;
+  isOpen = false;
   constructor() {}
 
   ngOnInit(): void {}

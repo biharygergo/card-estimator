ba2d7aad2468867808bfed3833a77b46ecb10290
Gergely Bihary <biharygergo@gmail.com>
2022-08-20 13:01:45 +0200

    feat: Zoom app improvements

--- functions/src/zoom/routes.ts
+++ functions/src/zoom/routes.ts
@@ -1,24 +1,26 @@
-import { contextHeader, getAppContext } from './cipher';
-import { getInstallURL, getToken, getDeeplink } from './zoomApi';
-import * as functions from 'firebase-functions';
-import { firestore } from 'firebase-admin';
+import {contextHeader, getAppContext} from "./cipher";
+import {getInstallURL, getToken, getDeeplink} from "./zoomApi";
+import * as functions from "firebase-functions";
+import {firestore} from "firebase-admin";
+import {getHost, isRunningInEmulator} from "../config";
 
 export const zoomHome = async (
-  req: functions.Request,
-  res: functions.Response
+    req: functions.Request,
+    res: functions.Response
 ): Promise<void> => {
   const header = req.header(contextHeader);
-  const appContext = getAppContext(header as string);
+  const isDev = isRunningInEmulator();
+  const appContext = getAppContext(header as string, isDev);
   const isZoom = header && appContext;
 
-  console.log('isZoom', isZoom);
+  const host = getHost(req);
   if (isZoom) {
-    let roomId = '';
+    let roomId = "";
     if (appContext.iid) {
       const db = firestore();
 
-      const roomsRef = db.collection('invitations');
-      const queryRef = roomsRef.where('invitationId', '==', appContext.iid);
+      const roomsRef = db.collection("invitations");
+      const queryRef = roomsRef.where("invitationId", "==", appContext.iid);
 
       const snapshot = await queryRef.get();
       if (snapshot.docs.length) {
@@ -27,49 +29,49 @@ export const zoomHome = async (
       }
     }
 
-    const isDev = req.query.dev;
-    const path = roomId ? `/${roomId}` : '/join';
-    const host =
-      isDev === '1'
-        ? 'https://72a6-80-99-77-114.eu.ngrok.io'
-        : 'https://planningpoker.live';
+    const path = roomId ? `/${roomId}` : "/join";
 
     const finalUrl = `${host}${path}?s=zoom`;
-    console.log('finalUrl', finalUrl);
+    console.log("finalUrl", finalUrl);
     return res.redirect(finalUrl);
   }
 
-  return res.redirect(
-    'https://us-central1-card-estimator.cloudfunctions.net/installZoomApp'
-  );
+  return res.redirect(`${host}/installZoomApp`);
 };
 
 export const installZoomApp = (
-  req: functions.Request,
-  res: functions.Response
+    req: functions.Request,
+    res: functions.Response
 ): void => {
-  const { url, state, verifier } = getInstallURL();
-  res.cookie('__session', JSON.stringify({ verifier, state }));
+  const isDev = isRunningInEmulator();
+  const {url, state, verifier} = getInstallURL(isDev, req);
+  console.log(isDev);
+  res.cookie("__session", JSON.stringify({verifier, state}));
   return res.redirect(url.href);
 };
 
 export const authorizeZoomApp = async (
-  req: functions.Request,
-  res: functions.Response
+    req: functions.Request,
+    res: functions.Response
 ): Promise<void> => {
   try {
-    const sessionCookie = req.cookies['__session'];
+    const sessionCookie = req.cookies["__session"];
     const parsedCookie = JSON.parse(sessionCookie);
 
     const verifier = parsedCookie.verifier;
 
     const code = req.query.code as string;
+    const isDev = isRunningInEmulator();
 
     console.log(`Got code: ${code}`);
     console.log(`Got verifier: ${verifier}`);
     // get Access Token from Zoom
-    const { access_token: accessToken } = await getToken(code, verifier);
-
+    const {access_token: accessToken} = await getToken(
+        code,
+        verifier,
+        isDev,
+        req
+    );
 
     // fetch deeplink from Zoom API
     const deeplink = await getDeeplink(accessToken);
@@ -79,10 +81,10 @@ export const authorizeZoomApp = async (
   } catch (e) {
     console.error(e);
     res
-      .status(500)
-      .send(
-        'Oh no, an error occured! Please try again or visit the web app at https://planningpoker.live. Error: ' +
-          ((e as any).message || (e as any).response.data)
-      );
+        .status(500)
+        .send(
+            "Oh no, an error occured! Please try again or visit the web app at https://planningpoker.live. Error: " +
+          ((e as any).message || e)
+        );
   }
 };
--- functions/src/zoom/zoomApi.ts
+++ functions/src/zoom/zoomApi.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import {URL} from "url";
-import {appConfig} from "../config";
+import {appConfig, getHost} from "../config";
 import * as crypto from "crypto";
+import * as functions from "firebase-functions";
 
 // returns a base64 encoded url
 const base64URL = (s: Buffer) =>
@@ -20,7 +21,7 @@ host.hostname = `api.${host.hostname}`;
 
 const baseURL = host.href;
 
-export function getInstallURL() {
+export function getInstallURL(isDev: boolean, request: functions.Request) {
   const state = rand("base64");
   const verifier = rand("ascii");
 
@@ -32,11 +33,17 @@ export function getInstallURL() {
 
   const challenge = base64URL(Buffer.from(digest));
 
+  const redirectUrl = getRedirectUrl(request);
+
+  console.log("redirectUrl", redirectUrl);
   const url = new URL("/oauth/authorize", zoomHost);
 
   url.searchParams.set("response_type", "code");
-  url.searchParams.set("client_id", appConfig.zoomClientId);
-  url.searchParams.set("redirect_uri", appConfig.zoomRedirectUrl);
+  url.searchParams.set(
+      "client_id",
+    isDev ? appConfig.zoomClientIdDev : appConfig.zoomClientId
+  );
+  url.searchParams.set("redirect_uri", redirectUrl);
   url.searchParams.set("code_challenge", challenge);
   url.searchParams.set("code_challenge_method", "S256");
   url.searchParams.set("state", state);
@@ -44,9 +51,11 @@ export function getInstallURL() {
   return {url, state, verifier};
 }
 
-function tokenRequest(params: any, id = "", secret = "") {
-  const username = id || appConfig.zoomClientId;
-  const password = secret || appConfig.zoomClientSecret;
+function tokenRequest(params: any, isDev: boolean) {
+  const username = isDev ? appConfig.zoomClientIdDev : appConfig.zoomClientId;
+  const password = isDev ?
+    appConfig.zoomClientSecretDev :
+    appConfig.zoomClientSecret;
 
   return axios({
     data: new URLSearchParams(params).toString(),
@@ -60,10 +69,12 @@ function tokenRequest(params: any, id = "", secret = "") {
       username,
       password,
     },
-  }).then(({data}) => Promise.resolve(data)).catch((error) => {
-    console.error("Axios error", error.response.data);
-    return Promise.reject(error.response.data);
-  });
+  })
+      .then(({data}) => Promise.resolve(data))
+      .catch((error) => {
+        console.error("Axios error", error.response.data);
+        return Promise.reject(error.response.data);
+      });
 }
 
 function apiRequest(
@@ -83,7 +94,19 @@ function apiRequest(
   }).then(({data}) => Promise.resolve(data));
 }
 
-export async function getToken(code: string, verifier: string) {
+function getRedirectUrl(request: functions.Request) {
+  const host = getHost(request);
+  const redirectUrl = `${host}${appConfig.zoomRedirectUrl}`;
+  console.log("redirectUrl", redirectUrl);
+  return redirectUrl;
+}
+
+export async function getToken(
+    code: string,
+    verifier: string,
+    isDev: boolean,
+    request: functions.Request
+) {
   if (!code || typeof code !== "string") {
     throw Error("authorization code must be a valid string");
   }
@@ -92,12 +115,17 @@ export async function getToken(code: string, verifier: string) {
     throw Error("code verifier code must be a valid string");
   }
 
-  return tokenRequest({
-    code,
-    code_verifier: verifier,
-    redirect_uri: appConfig.zoomRedirectUrl,
-    grant_type: "authorization_code",
-  });
+  const redirectUrl = getRedirectUrl(request);
+
+  return tokenRequest(
+      {
+        code,
+        code_verifier: verifier,
+        redirect_uri: redirectUrl,
+        grant_type: "authorization_code",
+      },
+      isDev
+  );
 }
 
 export function getDeeplink(token: string) {

af9c2c9c95a4e567ddd941010099c137a10d7740
Gergely Bihary <biharygergo@gmail.com>
2022-10-02 20:43:34 +0200

    feat: Join backLink

--- src/app/session-history-page/session-history-page.component.ts
+++ src/app/session-history-page/session-history-page.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-session-history-page',
   templateUrl: './session-history-page.component.html',
-  styleUrls: ['./session-history-page.component.scss']
+  styleUrls: ['./session-history-page.component.scss'],
 })
 export class SessionHistoryPageComponent implements OnInit {
+  backToPage = this.route.queryParamMap.pipe(
+    map((paramMap) => paramMap.get('backToPage'))
+  );
 
-  constructor() { }
+  constructor(private readonly route: ActivatedRoute) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

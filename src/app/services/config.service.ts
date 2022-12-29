50eb5dae0902104c97cd1a5e1c2a8172d5d0cebc
Gergely Bihary <biharygergo@gmail.com>
2022-12-29 09:33:38 +0100

    feat: Show account banner for returning users

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -9,6 +9,8 @@ import Cookies from 'js-cookie';
 
 /** Cookie key for the "Give feedback" button */
 export const FEEDBACK_FORM_FILLED_COOKIE_KEY = 'feedbackForm';
+/** Cookie key for hiding the permanent user banner in-game */
+export const HIDE_PERMANENT_ACCOUNT_BANNER_KEY = 'hidePermanentAccountBanner';
 
 @Injectable({
   providedIn: 'root',
@@ -23,12 +25,11 @@ export class ConfigService {
     return getBoolean(this.remoteConfig, key);
   }
 
-  setCookie(key: string, value: string) {
-    Cookies.set(key, value);
+  setCookie(key: string, value: string, expiresInDays: number = 365) {
+    Cookies.set(key, value, { expires: expiresInDays });
   }
 
-  getCookie(key): string|undefined {
+  getCookie(key): string | undefined {
     return Cookies.get(key);
   }
-
 }

06e1d12baa5a7e3125d1feebab1ac85a1df8fed6
Gergely Bihary <biharygergo@gmail.com>
2023-01-22 19:36:50 +0100

    try: Fix tests

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -15,6 +15,9 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-
+Cypress.on('uncaught:exception', (err, runnable) => {
+    console.error(err);
+    return false;
+});
 // Alternatively you can use CommonJS syntax:
 // require('./commands')

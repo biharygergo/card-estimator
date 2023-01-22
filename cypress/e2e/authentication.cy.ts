389539f7fe323c64000a54daf493a3a317a10139
Gergely Bihary <biharygergo@gmail.com>
2023-01-22 12:51:04 +0100

    feat: Add e2e tests for authentication

---
+++ cypress/e2e/authentication.cy.ts
@@ -0,0 +1,51 @@
+import { clearFirebaseLocalStorage, createNewRoom, setAppCheckCookie } from "../support/utils";
+
+describe('Creating a room as a new user', () => {
+    const testEmail = `test+${Date.now()}@company.com`;
+    const testPassword = 'SuperSecure123';
+
+    beforeEach(() => {
+      clearFirebaseLocalStorage();
+      setAppCheckCookie();
+      cy.visit('/create');
+      cy.contains('Got it').click();
+    });
+
+    it('can link an account with email', () => {
+        createNewRoom('Test Bela');
+        cy.get('#menu-button').click();
+        cy.get('#account-menu-item').click();
+
+        cy.get('#create-account-button-banner').click();
+    
+        // Account modal appears
+        cy.get('#email-input').click().type(testEmail);
+        cy.get('#password-input').click().type(testPassword);
+        cy.get('#create-account-button').click();
+        cy.wait(1000);
+
+        cy.get('#email-input').should('not.exist');
+        // Modal disappears
+        cy.get('#sign-up-modal').should('not.exist');
+
+        cy.get('#account-type-input').should('have.value', 'Permanent');
+    });
+
+    it('can sign back in', () => {
+        cy.get('#sign-in-button').click();
+
+        // Account modal appears
+        cy.get('#email-input').click().type(testEmail);
+        cy.get('#password-input').click().type(testPassword);
+        cy.get('#create-account-button').click();
+        cy.wait(1000);
+
+        cy.get('#email-input').should('not.exist');
+        // Modal disappears
+        cy.get('#sign-up-modal').should('not.exist');
+
+        cy.get('#welcome-back').should('contain.text', 'Welcome back, Test Bela!');
+
+    });
+
+});

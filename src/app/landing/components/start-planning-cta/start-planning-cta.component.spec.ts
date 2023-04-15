7775313241276f963c2ecb3b15cecb8c17f0b44c
Gergely Bihary <biharygergo@gmail.com>
2023-04-15 14:27:48 +0200

    feat: Redesign features page

---
+++ src/app/landing/components/start-planning-cta/start-planning-cta.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StartPlanningCtaComponent } from './start-planning-cta.component';
+
+describe('StartPlanningCtaComponent', () => {
+  let component: StartPlanningCtaComponent;
+  let fixture: ComponentFixture<StartPlanningCtaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ StartPlanningCtaComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(StartPlanningCtaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

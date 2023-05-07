1d0c06b85b7f29b25eddf7a06c7511f007c5dc8a
Gergely Bihary <biharygergo@gmail.com>
2023-05-07 13:36:56 +0200

    fix: Show Zoom banner on second visit

--- src/app/services/cookie.service.ts
+++ src/app/services/cookie.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ConfigService } from './config.service';
 
 export const COOKIE_ACCEPTED_KEY = 'cookiesAccepted';
+export const COOKIE_BEEN_HERE_BEFORE_KEY = 'beenHereBefore';
 
 @Injectable({
   providedIn: 'root',
@@ -9,14 +11,17 @@ export const COOKIE_ACCEPTED_KEY = 'cookiesAccepted';
 export class CookieService {
   showCookieBanner = true;
 
-  constructor(private snackBar: MatSnackBar) {}
+  constructor(
+    private snackBar: MatSnackBar,
+    private readonly configService: ConfigService
+  ) {}
 
   tryShowCookieBanner() {
     if (
       typeof window !== 'undefined' &&
       window?.localStorage &&
       !window.localStorage.getItem(COOKIE_ACCEPTED_KEY) &&
-      Intl.DateTimeFormat().resolvedOptions().timeZone.includes('Europe')
+      this.isLikelyInEurope()
     ) {
       const snackbarRef = this.snackBar.open(
         'This site uses cookies to analyze traffic and improve your experience. Read more about how cookies are used in our Privacy Policy.',
@@ -25,7 +30,18 @@ export class CookieService {
       snackbarRef.onAction().subscribe(() => {
         snackbarRef.dismiss();
         window.localStorage.setItem(COOKIE_ACCEPTED_KEY, 'true');
+        this.saveBeenHereBeforeCookie();
       });
+    } else if (!this.isLikelyInEurope) {
+      this.saveBeenHereBeforeCookie();
     }
+  }
+
+  saveBeenHereBeforeCookie() {
+    this.configService.setCookie(COOKIE_BEEN_HERE_BEFORE_KEY, 'true');
+  }
+
+  private isLikelyInEurope() {
+    return Intl.DateTimeFormat().resolvedOptions().timeZone.includes('Europe');
   }
 }

bb7d2c4bd5049ad54ebf490c9b66424cdecf40ab
Gergely Bihary <biharygergo@gmail.com>
2023-05-17 23:33:04 +0200

    fix: Test prod env

---
+++ src/environments/environment.test.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+    production: true,
+    firebase: {
+      apiKey: 'AIzaSyCeLm1QDx4B95QAS8t6FqaskkY7oTrwLu4',
+      authDomain: 'planning-poker-test-dc808.firebaseapp.com',
+      projectId: 'planning-poker-test-dc808',
+      storageBucket: 'planning-poker-test-dc808.appspot.com',
+      messagingSenderId: '615966905854',
+      appId: '1:615966905854:web:b2086e2a7cbef361755caf',
+      measurementId: 'G-EX57RZ95RH',
+    },
+    recaptcha3SiteKey: '6LdiDNIkAAAAALpZO5d0gm2tHRrjH0bG8VsOLJkT',
+    useEmulators: false,
+    premiumPriceId: 'price_1MhxFgCG1hllVHnccUeyTdNX'
+  };
+  

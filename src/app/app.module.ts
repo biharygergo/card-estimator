d45775750248fa097cb38d152c3eb02fe5038e62
Gergely Bihary <biharygergo@gmail.com>
2023-06-02 10:08:11 +0200

    fix: Clean up AppCheck

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,8 @@ import {
   NgModule,
   APP_INITIALIZER,
   ErrorHandler,
-  InjectionToken,
-  Optional,
 } from '@angular/core';
 import * as Sentry from '@sentry/angular-ivy';
-import Cookies from 'js-cookie';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,23 +22,12 @@ import {
   provideAnalytics,
   ScreenTrackingService,
 } from '@angular/fire/analytics';
-import {
-  AppCheckToken,
-  CustomProvider,
-  initializeAppCheck,
-  provideAppCheck,
-  ReCaptchaV3Provider,
-} from '@angular/fire/app-check';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import type { app } from 'firebase-admin';
-export const FIREBASE_ADMIN = new InjectionToken<app.App>('firebase-admin');
 
-import { getFunctions, httpsCallable } from 'firebase/functions';
+import { getFunctions } from 'firebase/functions';
 
 import { environment } from '../environments/environment';
 import { Router } from '@angular/router';
-import { isRunningInZoom } from './utils';
-import { initializeApp as originalInitializeApp } from 'firebase/app';
 import { RoomLoadingComponent } from './room-loading/room-loading.component';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { AppConfigModule } from './app-config.module';
@@ -58,41 +44,6 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { GlobalErrorHandler } from './error-handler';
 import { HttpClientModule } from '@angular/common/http';
 
-let appCheckToken: AppCheckToken;
-type FetchAppCheckTokenData = { token: string; expiresAt: number };
-
-function fetchToken(): Promise<AppCheckToken> {
-  originalInitializeApp(environment.firebase);
-  const functions = getFunctions();
-  const fetchAppCheckToken = httpsCallable<undefined, FetchAppCheckTokenData>(
-    functions,
-    'fetchAppCheckToken'
-  );
-  return fetchAppCheckToken().then((response) => {
-    return {
-      token: response.data.token,
-      expireTimeMillis: response.data.expiresAt * 1000,
-    };
-  });
-}
-
-function loadAppConfig(): Promise<any> {
-  return new Promise((resolve, reject) => {
-    if (isRunningInZoom()) {
-      fetchToken()
-        .then((token) => {
-          appCheckToken = token;
-          resolve(undefined);
-        })
-        .catch((e) => {
-          console.error(e);
-          resolve(undefined);
-        });
-    } else {
-      resolve(undefined);
-    }
-  });
-}
 @NgModule({
   declarations: [AppComponent, RoomLoadingComponent],
   imports: [
@@ -142,66 +93,6 @@ function loadAppConfig(): Promise<any> {
       }
       return functions;
     }),
-    provideAppCheck(
-      (injector) => {
-        const admin = injector.get<app.App | null>(FIREBASE_ADMIN, null);
-        if (admin) {
-          const provider = new CustomProvider({
-            getToken: () =>
-              admin
-                .appCheck()
-                .createToken(environment.firebase.appId, {
-                  ttlMillis: 604_800_000 /* 1 week */,
-                })
-                .then(({ token, ttlMillis: expireTimeMillis }) => ({
-                  token,
-                  expireTimeMillis,
-                })),
-          });
-          return initializeAppCheck(undefined, {
-            provider,
-            isTokenAutoRefreshEnabled: false,
-          });
-        } else {
-          let provider: ReCaptchaV3Provider | CustomProvider;
-          if (isRunningInZoom()) {
-            provider = new CustomProvider({
-              getToken: () =>
-                new Promise((resolve) => {
-                  window.setTimeout(() => {
-                    // Workaround for not being able to refresh the AppCheck token here...
-                    window.location.reload();
-                  }, appCheckToken.expireTimeMillis - Date.now());
-                  resolve(appCheckToken);
-                }),
-            });
-          } else {
-            provider = new ReCaptchaV3Provider(environment.recaptcha3SiteKey);
-          }
-
-          if (
-            !environment.production ||
-            (typeof window !== 'undefined' &&
-              window.origin.includes('localhost'))
-          ) {
-            if (
-              typeof window !== 'undefined' &&
-              window.origin.includes('4200')
-            ) {
-              (self as any).FIREBASE_APPCHECK_DEBUG_TOKEN =
-                Cookies.get('APP_CHECK_CI_TOKEN');
-            } else {
-              (self as any).FIREBASE_APPCHECK_DEBUG_TOKEN = true;
-            }
-          }
-          return initializeAppCheck(undefined, {
-            provider,
-            isTokenAutoRefreshEnabled: true,
-          });
-        }
-      },
-      [new Optional(), FIREBASE_ADMIN]
-    ),
   ],
   providers: [
     ScreenTrackingService,
@@ -217,11 +108,6 @@ function loadAppConfig(): Promise<any> {
       provide: APP_INITIALIZER,
       useFactory: () => () => {},
       deps: [Sentry.TraceService],
-      multi: true,
-    },
-    {
-      provide: APP_INITIALIZER,
-      useFactory: () => loadAppConfig,
       multi: true,
     },
     provideClientHydration(),

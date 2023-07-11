ba174775e11092602021950363e9e731514a9662
Gergely Bihary <biharygergo@gmail.com>
2023-07-11 19:41:57 +0200

    feat: Initial changes for Teams

---
+++ src/app/integrations/teams/configure-tab/configure-tab.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConfigureTabComponent } from './configure-tab.component';
+
+describe('ConfigureTabComponent', () => {
+  let component: ConfigureTabComponent;
+  let fixture: ComponentFixture<ConfigureTabComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ConfigureTabComponent]
+    });
+    fixture = TestBed.createComponent(ConfigureTabComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/integrations/teams/configure-tab/configure-tab.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { app, pages } from "@microsoft/teams-js";
+
+@Component({
+  selector: 'app-configure-tab',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './configure-tab.component.html',
+  styleUrls: ['./configure-tab.component.scss']
+})
+export class ConfigureTabComponent implements OnInit {
+
+  ngOnInit(): void {
+     // Initialize the Microsoft Teams SDK
+     app.initialize().then(() => {
+      /**
+       * When the user clicks "Save", save the url for your configured tab.
+       * This allows for the addition of query string parameters based on
+       * the settings selected by the user.
+       */
+      pages.config.registerOnSaveHandler((saveEvent) => {
+        const baseUrl = `https://${window.location.hostname}:${window.location.port}`;
+        pages.config
+          .setConfig({
+            suggestedDisplayName: "Planning Poker",
+            entityId: "Test",
+            contentUrl: baseUrl + "/join",
+            websiteUrl: baseUrl + "/",
+          })
+          .then(() => {
+            saveEvent.notifySuccess();
+          });
+      });
+
+      /**
+       * After verifying that the settings for your tab are correctly
+       * filled in by the user you need to set the state of the dialog
+       * to be valid.  This will enable the save button in the configuration
+       * dialog.
+       */
+      pages.config.setValidityState(true);
+    });
+  }
+
+}

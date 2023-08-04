f4f05a9c5fbaff61d93a011931f8d6286d3b5fd5
Gergely Bihary <biharygergo@gmail.com>
2023-08-04 23:00:35 +0200

    feat: Dark mode

---
+++ src/app/services/theme.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { ConfigService } from './config.service';
+import { map } from 'rxjs/operators';
+
+export enum Theme {
+  DARK = 'dark',
+  DEFAULT = 'default',
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeService {
+  currentTheme = Theme.DEFAULT;
+  themeChanged = new Subject<void>();
+
+  constructor(private readonly configService: ConfigService) {
+    const themeFromCookie = this.configService.getCookie('preferredTheme');
+    if (
+      themeFromCookie &&
+      Object.values(Theme).includes(themeFromCookie as Theme) &&
+      themeFromCookie !== this.currentTheme
+    ) {
+      this.currentTheme = themeFromCookie as Theme;
+      this.themeChanged.next();
+    }
+  }
+
+  setTheme(theme: Theme) {
+    this.currentTheme = theme;
+    this.themeChanged.next();
+    this.configService.setCookie('preferredTheme', theme);
+  }
+}

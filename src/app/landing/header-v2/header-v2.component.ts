dea291cdff2825fafa3478c301b07b84e9c97425
Gergely Bihary <biharygergo@gmail.com>
2024-02-25 20:15:55 +0100

    feat: Improve mobile nav header

--- src/app/landing/header-v2/header-v2.component.ts
+++ src/app/landing/header-v2/header-v2.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { APP_CONFIG, AppConfig } from 'src/app/app-config.module';
 import { CookieService } from 'src/app/services/cookie.service';
+import { NavigationService } from 'src/app/services/navigation.service';
 
 @Component({
   selector: 'planning-poker-landing-header',
   templateUrl: './header-v2.component.html',
-  styleUrls: ['./header-v2.component.scss']
+  styleUrls: ['./header-v2.component.scss'],
 })
 export class HeaderV2Component implements OnInit {
   isOpen = false;
   menuOpen = false;
 
-  constructor(private readonly cookieService: CookieService) {}
+  isEmbeddedApp = this.config.runningIn !== 'web';
+
+  constructor(
+    private readonly cookieService: CookieService,
+    @Inject(APP_CONFIG) public config: AppConfig,
+    private readonly historyService: NavigationService
+  ) {}
 
   ngOnInit(): void {
     this.cookieService.tryShowCookieBanner();
@@ -24,4 +32,7 @@ export class HeaderV2Component implements OnInit {
     this.isOpen = false;
   }
 
+  goBack() {
+    this.historyService.back();
+  }
 }

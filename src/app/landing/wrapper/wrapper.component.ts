36f79629afc53571c251a0af6bd1943b8afda120
Gergely Bihary <biharygergo@gmail.com>
2024-08-25 22:48:12 +0200

    feat: Improve footer

--- src/app/landing/wrapper/wrapper.component.ts
+++ src/app/landing/wrapper/wrapper.component.ts
@@ -2,19 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { ZoomAppBannerComponent } from '../../shared/zoom-app-banner/zoom-app-banner.component';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { HeaderV2Component } from '../header-v2/header-v2.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
-    selector: 'app-wrapper',
-    templateUrl: './wrapper.component.html',
-    styleUrls: ['./wrapper.component.scss'],
-    standalone: true,
-    imports: [HeaderV2Component, RouterOutlet, RouterLink, ZoomAppBannerComponent]
+  selector: 'app-wrapper',
+  templateUrl: './wrapper.component.html',
+  styleUrls: ['./wrapper.component.scss'],
+  standalone: true,
+  imports: [
+    HeaderV2Component,
+    RouterOutlet,
+    RouterLink,
+    ZoomAppBannerComponent,
+    NgOptimizedImage,
+  ],
 })
 export class WrapperComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

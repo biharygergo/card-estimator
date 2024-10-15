4032caa5207743f99137e1256399b361a7eb6d85
Gergely Bihary <biharygergo@gmail.com>
2024-10-15 11:22:08 +0000

    feat: Refactor imports

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,56 +10,6 @@ import {appCheck} from "firebase-admin";
 import * as cookieParser from "cookie-parser";
 import * as Sentry from "@sentry/node";
 
-import {
-  authorizeZoomApp,
-  generateCodeChallenge,
-  inClientOnAuthorized,
-  installZoomApp,
-  zoomHome,
-} from "./zoom/routes";
-import {
-  googleAuthSuccess,
-  startGoogleOauthFlow,
-  zoomAuthSuccess,
-} from "./zoom/googleAuth";
-import {
-  onUserDetailsCreate,
-  onUserDetailsUpdate,
-} from "./profile/onUserCreateUpdate";
-import {getFirestore} from "firebase-admin/firestore";
-import {
-  enterProtectedRoom,
-  setRoomPassword,
-} from "./room/password-protection";
-import {
-  acceptInvitation,
-  onOrganizationInviteCreated,
-} from "./organizations/invitation";
-import {onOrganizationUpdated} from "./organizations/protection";
-import {
-  onCustomerPaymentCreated,
-  onCustomerPaymentUpdated,
-  onCustomerSubscriptionCreated,
-  onCustomerSubscriptionUpdated,
-} from "./customers/subscription";
-import {startJiraAuthFlow, onJiraAuthorizationReceived} from "./jira/oauth";
-import {searchJira} from "./jira/search";
-import {captureError} from "./shared/errors";
-import {createSummary} from "./summary";
-import {onRoomCreated} from "./room/created";
-import {IssueUpdateRequestData, updateIssue} from "./jira/updateIssue";
-import {onTeamsGoogleAuthResult, startTeamsGoogleAuth} from "./ms-teams";
-import {createRoom} from "./room/new-room";
-import {assignCreditsAsNeeded, getAllCreditBundles} from "./credits";
-import {
-  onLinearAuthorizationReceived,
-  startLinearAuthFlow,
-} from "./linear/oauth";
-import {searchLinear} from "./linear/search";
-import {updateLinearIssue} from "./linear/updateIssue";
-import {updateRoomsTableInBigQuery} from "./room/bigquery";
-import {onOAuthResult, startOAuth} from "./oauth";
-
 initializeApp();
 getFirestore().settings({ignoreUndefinedProperties: true});
 
@@ -69,14 +19,19 @@ Sentry.init({
 });
 
 exports.authorizeZoomApp = onRequest({cors: true}, async (req, res) => {
+  const {authorizeZoomApp} = await import("./zoom/routes");
   cookieParser()(req, res, async () => authorizeZoomApp(req, res));
 });
 
 exports.zoomHome = onRequest({cors: true}, async (req, res) => {
+  const {zoomHome} = await import("./zoom/routes");
   cookieParser()(req, res, () => zoomHome(req, res));
 });
 
-exports.installZoomApp = onRequest({cors: true}, installZoomApp);
+exports.installZoomApp = onRequest({cors: true}, async (req, res) => {
+  const {installZoomApp} = await import("./zoom/routes");
+  installZoomApp(req, res);
+});
 
 exports.uninstallZoomApp = onRequest({cors: true}, async (req) => {
   console.log("Zoom App Uninstallation");
@@ -84,43 +39,42 @@ exports.uninstallZoomApp = onRequest({cors: true}, async (req) => {
 });
 
 exports.fetchAppCheckToken = onCall({cors: true}, async () => {
-  const appId = "1:417578634660:web:3617c13e4d28109aa18531";
   try {
-    const result = await appCheck().createToken(appId);
+    const result = await appCheck().createToken("1:417578634660:web:3617c13e4d28109aa18531");
     const expiresAt = Math.floor(Date.now() / 1000) + 60 * 60;
     return {token: result.token, expiresAt};
   } catch (err) {
     console.error("Unable to create App Check token.", err);
+    const {captureError} = await import("./shared/errors");
     captureError(err);
-    return "An error occured, please check the logs.";
+    return "An error occurred, please check the logs.";
   }
 });
 
 exports.generateCodeChallenge = onRequest({cors: true}, async (req, res) => {
+  const {generateCodeChallenge} = await import("./zoom/routes");
   cookieParser()(req, res, () => generateCodeChallenge(req, res));
 });
 
 exports.inClientOnAuthorized = onRequest({cors: true}, async (req, res) => {
+  const {inClientOnAuthorized} = await import("./zoom/routes");
   cookieParser()(req, res, () => inClientOnAuthorized(req, res));
 });
 
 exports.startGoogleAuth = onRequest({cors: true}, async (req, res) => {
+  const {startGoogleOauthFlow} = await import("./zoom/googleAuth");
   cookieParser()(req, res, () => startGoogleOauthFlow(req, res));
 });
 
-exports.onZoomAuthResponseRedirectToGoogle = onRequest(
-    {cors: true},
-    async (req, res) => {
-      cookieParser()(req, res, () => zoomAuthSuccess(req, res));
-    }
-);
+exports.onZoomAuthResponseRedirectToGoogle = onRequest({cors: true}, async (req, res) => {
+  const {zoomAuthSuccess} = await import("./zoom/googleAuth");
+  cookieParser()(req, res, () => zoomAuthSuccess(req, res));
+});
 
-exports.onGoogleAuthResponseDeeplink = onRequest(
-    {cors: true},
-    async (req, res) => {
-      cookieParser()(req, res, () => googleAuthSuccess(req, res));
-    }
-);
+exports.onGoogleAuthResponseDeeplink = onRequest({cors: true}, async (req, res) => {
+  const {googleAuthSuccess} = await import("./zoom/googleAuth");
+  cookieParser()(req, res, () => googleAuthSuccess(req, res));
+});
 
 exports.giveFeedback = onRequest({cors: true}, async (req, res) => {
   res.redirect("https://forms.gle/Rhd8mAQqCmewhfCR7");
@@ -128,95 +82,97 @@ exports.giveFeedback = onRequest({cors: true}, async (req, res) => {
 
 exports.reportAnIssue = onRequest({cors: true}, async (req, res) => {
   res.redirect(
-      // eslint-disable-next-line max-len
-      "mailto:info@planningpoker.live?subject=Issue%20Report%20-%20Planning%20Poker&body=Dear%20Developers%2C%0D%0A%0D%0AI%20have%20run%20into%20the%20following%20issue%20while%20using%20the%20Planning%20Poker%20app%3A%0D%0A%0D%0A%3CDescribe%20your%20issue%20in%20detail%20here%3E%0D%0A%0D%0AHave%20a%20great%20day%2C%0D%0A%3CYour%20name%3E"
+    "mailto:info@planningpoker.live?subject=Issue%20Report%20-%20Planning%20Poker&body=Dear%20Developers%2C%0D%0A%0D%0AI%20have%20run%20into%20the%20following%20issue%20while%20using%20the%20Planning%20Poker%20app%3A%0D%0A%0D%0A%3CDescribe%20your%20issue%20in%20detail%20here%3E%0D%0A%0D%0AHave%20a%20great%20day%2C%0D%0A%3CYour%20name%3E"
   );
 });
 
 exports.sendEmail = onRequest({cors: true}, async (req, res) => {
-  const subject = encodeURIComponent(
-      (req.query.subject as string | undefined) || ""
-  );
-  res.redirect(
-      // eslint-disable-next-line max-len
-      `mailto:info@planningpoker.live?subject=${subject}`
-  );
+  const subject = encodeURIComponent((req.query.subject as string | undefined) || "");
+  res.redirect(`mailto:info@planningpoker.live?subject=${subject}`);
 });
 
-exports.onUserDetailsCreate = onDocumentCreated(
-    "userDetails/{userId}",
-    onUserDetailsCreate
-);
+exports.onUserDetailsCreate = onDocumentCreated("userDetails/{userId}", async (snap, context) => {
+  const {onUserDetailsCreate} = await import("./profile/onUserCreateUpdate");
+  onUserDetailsCreate(snap, context);
+});
 
-exports.onUserDetailsUpdate = onDocumentUpdated(
-    "userDetails/{userId}",
-    onUserDetailsUpdate
-);
+exports.onUserDetailsUpdate = onDocumentUpdated("userDetails/{userId}", async (change, context) => {
+  const {onUserDetailsUpdate} = await import("./profile/onUserCreateUpdate");
+  onUserDetailsUpdate(change, context);
+});
 
-exports.setRoomPassword = onCall({cors: true}, async (request) =>
-  setRoomPassword(request)
-);
+exports.setRoomPassword = onCall({cors: true}, async (request) => {
+  const {setRoomPassword} = await import("./room/password-protection");
+  setRoomPassword(request);
+});
 
-exports.enterProtectedRoom = onCall({cors: true}, async (request) =>
-  enterProtectedRoom(request)
-);
+exports.enterProtectedRoom = onCall({cors: true}, async (request) => {
+  const {enterProtectedRoom} = await import("./room/password-protection");
+  enterProtectedRoom(request);
+});
 
-exports.onOrganizationUpdated = onDocumentUpdated(
-    "organizations/{organizationId}",
-    onOrganizationUpdated
-);
+exports.onOrganizationUpdated = onDocumentUpdated("organizations/{organizationId}", async (change, context) => {
+  const {onOrganizationUpdated} = await import("./organizations/protection");
+  onOrganizationUpdated(change, context);
+});
 
-exports.onOrganizationInvitation = onDocumentCreated(
-    "organizations/{organizationId}/memberInvitations/{invitationId}",
-    onOrganizationInviteCreated
-);
+exports.onOrganizationInvitation = onDocumentCreated("organizations/{organizationId}/memberInvitations/{invitationId}", async (snap, context) => {
+  const {onOrganizationInviteCreated} = await import("./organizations/invitation");
+  onOrganizationInviteCreated(snap, context);
+});
 
-exports.acceptOrganizationInvitation = onRequest(
-    {cors: true},
-    async (req, res) => {
-      cookieParser()(req, res, () => acceptInvitation(req));
-    }
-);
+exports.acceptOrganizationInvitation = onRequest({cors: true}, async (req, res) => {
+  const {acceptInvitation} = await import("./organizations/invitation");
+  cookieParser()(req, res, () => acceptInvitation(req));
+});
 
-exports.onUserSubscriptionCreated = onDocumentCreated(
-    "customers/{customerId}/subscriptions/{subscriptionId}",
-    onCustomerSubscriptionCreated
-);
+exports.onUserSubscriptionCreated = onDocumentCreated("customers/{customerId}/subscriptions/{subscriptionId}", async (snap, context) => {
+  const {onCustomerSubscriptionCreated} = await import("./customers/subscription");
+  onCustomerSubscriptionCreated(snap, context);
+});
 
-exports.onUserSubscriptionUpdated = onDocumentUpdated(
-    "customers/{customerId}/subscriptions/{subscriptionId}",
-    onCustomerSubscriptionUpdated
-);
+exports.onUserSubscriptionUpdated = onDocumentUpdated("customers/{customerId}/subscriptions/{subscriptionId}", async (change, context) => {
+  const {onCustomerSubscriptionUpdated} = await import("./customers/subscription");
+  onCustomerSubscriptionUpdated(change, context);
+});
 
 exports.startJiraAuth = onRequest({cors: true}, async (req, res) => {
+  const {startJiraAuthFlow} = await import("./jira/oauth");
   cookieParser()(req, res, () => startJiraAuthFlow(req, res));
 });
 
 exports.onJiraAuthResponse = onRequest({cors: true}, async (req, res) => {
+  const {onJiraAuthorizationReceived} = await import("./jira/oauth");
   cookieParser()(req, res, () => onJiraAuthorizationReceived(req, res));
 });
 
-exports.queryJiraIssues = onCall({cors: true}, async (request) =>
-  searchJira(request)
-);
+exports.queryJiraIssues = onCall({cors: true}, async (request) => {
+  const {searchJira} = await import("./jira/search");
+  searchJira(request);
+});
 
-exports.updateIssue = onCall({cors: true}, async (req) =>
+exports.updateIssue = onCall({cors: true}, async (req) => {
+  const {updateIssue} = await import("./jira/updateIssue");
+  const {updateLinearIssue} = await import("./linear/updateIssue");
   (req.data.updateRequest as IssueUpdateRequestData).provider === "linear" ?
     updateLinearIssue(req) :
-    updateIssue(req)
-);
+    updateIssue(req);
+});
 
 exports.startLinearAuth = onRequest({cors: true}, async (req, res) => {
+  const {startLinearAuthFlow} = await import("./linear/oauth");
   cookieParser()(req, res, () => startLinearAuthFlow(req, res));
 });
 
 exports.onLinearAuthResponse = onRequest({cors: true}, async (req, res) => {
+  const {onLinearAuthorizationReceived} = await import("./linear/oauth");
   cookieParser()(req, res, () => onLinearAuthorizationReceived(req, res));
 });
 
-exports.queryLinearIssues = onCall({cors: true}, async (request) =>
-  searchLinear(request)
-);
+exports.queryLinearIssues = onCall({cors: true}, async (request) => {
+  const {searchLinear} = await import("./linear/search");
+  searchLinear(request);
+});
 
 exports.safeRedirect = onRequest({cors: true}, async (req, res) => {
   const redirectTo = req.query.redirectTo;
@@ -226,65 +182,63 @@ exports.safeRedirect = onRequest({cors: true}, async (req, res) => {
   return;
 });
 
-exports.createSummary = onCall({cors: true}, async (req) =>
-  createSummary(req)
-);
+exports.createSummary = onCall({cors: true}, async (req) => {
+  const {createSummary} = await import("./summary");
+  createSummary(req);
+});
 
-exports.onRoomCreated = onDocumentCreated("rooms/{roomId}", onRoomCreated);
+exports.onRoomCreated = onDocumentCreated("rooms/{roomId}", async (snap, context) => {
+  const {onRoomCreated} = await import("./room/created");
+  onRoomCreated(snap, context);
+});
 
 exports.startTeamsGoogleAuth = onRequest({cors: true}, async (req, res) => {
+  const {startTeamsGoogleAuth} = await import("./ms-teams");
   cookieParser()(req, res, () => startTeamsGoogleAuth(req, res));
 });
 
-exports.onTeamsGoogleAuthResult = onRequest(
-    {cors: true},
-    async (req, res) => {
-      cookieParser()(req, res, () => onTeamsGoogleAuthResult(req, res));
-    }
-);
+exports.onTeamsGoogleAuthResult = onRequest({cors: true}, async (req, res) => {
+  const {onTeamsGoogleAuthResult} = await import("./ms-teams");
+  cookieParser()(req, res, () => onTeamsGoogleAuthResult(req, res));
+});
 
 exports.startOAuth = onRequest({cors: true}, async (req, res) => {
+  const {startOAuth} = await import("./oauth");
   cookieParser()(req, res, () => startOAuth(req, res));
 });
 
-exports.onOAuthResult = onRequest(
-    {cors: true},
-    async (req, res) => {
-      cookieParser()(req, res, () => onOAuthResult(req, res));
-    }
-);
+exports.onOAuthResult = onRequest({cors: true}, async (req, res) => {
+  const {onOAuthResult} = await import("./oauth");
+  cookieParser()(req, res, () => onOAuthResult(req, res));
+});
 
-exports.createRoom = onCall({cors: true}, createRoom);
-exports.getAllCreditsAndAssignWelcome = onCall(
-    {cors: true},
-    async (request) => {
-      if (!request.auth?.uid) {
-        throw new HttpsError(
-            "unauthenticated",
-            "You need to be authenticated to fetch credits."
-        );
-      }
-      await assignCreditsAsNeeded(request.auth.uid);
+exports.createRoom = onCall({cors: true}, async (req) => {
+  const {createRoom} = await import("./room/new-room");
+  createRoom(req);
+});
 
-      return getAllCreditBundles(request.auth.uid);
-    }
-);
+exports.getAllCreditsAndAssignWelcome = onCall({cors: true}, async (request) => {
+  if (!request.auth?.uid) {
+    throw new HttpsError("unauthenticated", "You need to be authenticated to fetch credits.");
+  }
+  const {assignCreditsAsNeeded, getAllCreditBundles} = await import("./credits");
+  await assignCreditsAsNeeded(request.auth.uid);
+  return getAllCreditBundles(request.auth.uid);
+});
 
-exports.onUserPaymentCreated = onDocumentCreated(
-    "customers/{customerId}/payments/{paymentId}",
-    onCustomerPaymentCreated
-);
+exports.onUserPaymentCreated = onDocumentCreated("customers/{customerId}/payments/{paymentId}", async (snap, context) => {
+  const {onCustomerPaymentCreated} = await import("./customers/subscription");
+  onCustomerPaymentCreated(snap, context);
+});
 
-exports.onUserPaymentUpdated = onDocumentUpdated(
-    "customers/{customerId}/payments/{paymentId}",
-    onCustomerPaymentUpdated
-);
+exports.onUserPaymentUpdated = onDocumentUpdated("customers/{customerId}/payments/{paymentId}", async (change, context) => {
+  const {onCustomerPaymentUpdated} = await import("./customers/subscription");
+  onCustomerPaymentUpdated(change, context);
+});
 
-exports.updateRoomsTableInBigQuery = onSchedule(
-    {schedule: "every day 06:00", timeoutSeconds: 1000, memory: "1GiB"},
-    async () => {
-      console.log("Scheduled room update function triggered.");
-      await updateRoomsTableInBigQuery();
-      console.log("Room update completed.");
-    }
-);
+exports.updateRoomsTableInBigQuery = onSchedule({schedule: "every day 06:00", timeoutSeconds: 1000, memory: "1GiB"}, async () => {
+  console.log("Scheduled room update function triggered.");
+  const {updateRoomsTableInBigQuery} = await import("./room/bigquery");
+  await updateRoomsTableInBigQuery();
+  console.log("Room update completed.");
+});

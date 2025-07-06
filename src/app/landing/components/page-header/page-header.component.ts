7e7c7ef27ef1a5596fb9a50142abfb89713ae00a
Gergely Bihary <gergely@elevenlabs.io>
2025-07-06 15:43:21 +0200

    fix: sizing

--- src/app/landing/components/page-header/page-header.component.ts
+++ src/app/landing/components/page-header/page-header.component.ts
@@ -4,6 +4,10 @@ import { Component, Input, OnInit } from '@angular/core';
   selector: 'app-page-header',
   template: `
     <header>
+      <div class="corner-decoration top-left"></div>
+      <div class="corner-decoration top-right"></div>
+      <div class="corner-decoration bottom-left"></div>
+      <div class="corner-decoration bottom-right"></div>
       <div class="title" [class.centered]="centered">
         <h1>{{ title }}</h1>
         @if (subtitle) {
